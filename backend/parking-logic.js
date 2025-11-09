7e428e21b70ce97c85de7de55ff159f36630d9b7
Nestasss <141137180+Nestasss@users.noreply.github.com>
2025-11-09 18:43:07 +0300

    Update parking-logic.js

--- backend/parking-logic.js
+++ backend/parking-logic.js
@@ -1,1 +1,13 @@
+// Логика определения парковки
+// TODO: Реализовать проверку скорости, времени остановки и геоданных
 
+function isParking(stopData) {
+  // stopData = { speed, duration, location }
+  // Пока упрощённая логика: скорость < 5 км/ч и остановка > 2 минут
+  if (stopData.speed < 5 && stopData.duration > 120) {
+    return true;
+  }
+  return false;
+}
+
+module.exports = { isParking };
